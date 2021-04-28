cc1f5ccdee68e3a3d4f688068b0ca276a7202d20
emna7 <bhmemna7@gmail.com>
2021-04-29 00:04:27 +0100

    create event

--- iwi/src/Event.js
+++ iwi/src/Event.js
@@ -1,36 +1,204 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import AddIcon from '@material-ui/icons/Add';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Radio from '@material-ui/core/Radio'
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import DateFnsUtils from '@date-io/date-fns';
 
-function Event() {
+import {
+    MuiPickersUtilsProvider,
+    KeyboardTimePicker,
+    KeyboardDatePicker,
+  } from '@material-ui/pickers';
+
+
+function Copyright() {
   return (
-    <div className="Event">
-      <div className="Event-header">
-        <h1>
-          Create-Event
-        </h1>
-      </div>
-      <div className="Event-body">
-        <div className="Event">
-          <h3>Sign Up For Free</h3>
-          <label htmlFor="firstname">FirstName: </label>
-          <input type="firstname" id="firstname" name="firstname" />
-          <label htmlFor="lastname">LastName: </label>
-          <input type="lastname" id="lastname" name="lastname" />
-          <label htmlFor="gender">Gender: </label>
-          <input type="gender" id="gender" name="gender" />
-          <label htmlFor="phonenumber">Phone Number: </label>
-          <input type="ponenumber" id="phonenumber" name="phonenumber" />
-          <label htmlFor="email">Email: </label>
-          <input type="email" id="email" name="email" />
-          <label htmlFor="password">Set a Password: </label>
-          <input type="password" id="password" name="password" />
-          <button>OK</button>
-        </div>
-      </div>
-      <div className="Event-footer">
-      </div>
-    </div>
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
 
-export default Event;
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    // alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  FormLabel: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(1)
+  },
+
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+
+  formControl: {
+    marginTop: theme.spacing(1),
+    width: "50%",
+    marginBottom: theme.spacing(3),
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+export default function Event() {
+  const [paid, setPaid] = React.useState('paid');
+
+//   const handleChangePaid = (event) => {
+//     setPaid(event.target.paid);
+//   };
+
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState('panel1');
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+  const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Container align="center" >
+        <Avatar align="center" className={classes.avatar}>
+          <AddIcon />
+        </Avatar>
+        </Container>
+        <Box textAlign="center">
+        <Typography  component="h1" variant="h5">
+          Create Event
+        </Typography>
+        </Box>
+        <FormLabel className={classes.FormLabel} component="legend">Location:</FormLabel>
+
+        <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="outlined-basic" variant="outlined" />
+        </form>
+          <FormControl className={classes.FormLabel} component="fieldset">
+            <FormLabel component="legend">Paid:</FormLabel>
+            <RadioGroup row aria-label="events" name="events1" value={paid} onChange={handleChange('yes')}>
+              <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+              <FormControlLabel value="no" control={<Radio />} label="No" />
+            </RadioGroup>
+          </FormControl>
+          <Accordion square expanded={expanded === 'yes'}>
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+          <Typography>Collapsible Group Item #1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <FormLabel className={classes.FormLabel} component="legend">Date/Time:</FormLabel>
+  
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container justify="space-around">
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Start date"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+        <KeyboardDatePicker
+          variant="inline"
+          disableToolbar
+          margin="normal"
+          id="date-picker-dialog"
+          label="End date"
+          format="MM/dd/yyyy"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+        <KeyboardTimePicker
+          margin="normal"
+          id="time-picker"
+          label="Time"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change time',
+          }}
+        />
+      </Grid>
+    </MuiPickersUtilsProvider>
+
+    <FormLabel className={classes.FormLabel} component="legend">Description:</FormLabel>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Create
+          </Button>
+          
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
